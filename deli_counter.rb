4d2d2c9d04ee4077dcff4a0cd1f35a514c18aafe
Jessica Cervantes <cervantesjess1994@gmail.com>
2019-09-19 11:07:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ katz_deli = []
 def take_a_number(array, name)
   array.push(name)
   position = array.index(name)
-  puts "Welcome, #{name}. Your number is #{array.index(name)+1}."
+  puts "Welcome, #{name}. Your number is #{array.index(name)+1} in line."
   return name, position
 end
 
