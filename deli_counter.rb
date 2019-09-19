a459516af1aaa1ca5e13ce1cdfc0bbe4bd679a3e
Jessica Cervantes <cervantesjess1994@gmail.com>
2019-09-19 10:52:27 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,3 +16,11 @@ def take_a_number(line,new_person)
   line.push(new_person)
   puts "Welcome, #{new_person}. Your number is #{line.length}."
 end
+
+def now_serving(line)
+  if 
+
+  else line.length == 0
+    puts "There is nobody waiting to be served!"
+  end
+end
