38f9f9f8f7d33a6389bcde613037531f55389239
Jessica Cervantes <cervantesjess1994@gmail.com>
2019-09-19 10:50:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,18 @@
-# Write your code here.
+def line(array)
+   if array.length >= 1
+      current_line = []
+      counter = 1
+      arra.each do |name|
+        current_line.push("#{counter}. #{name}")
+        counter += 1
+      end
+      puts "The line is currently: #{current_line.join(" ")}"
+    else
+      puts "The line is currently empty."
+    end
+end
+
+def take_a_number(line,new_person)
+  line.push(new_person)
+  puts "Welcome, #{new_person}. Your number is #{line.length}."
+end
